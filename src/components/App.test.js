994aa90edb23ea9c656e7fbc680b4cdc50425aad
Miles Kingston <miles.kingston@dixonscarphone.com>
2017-07-24 12:50:43 +0100

    more unit test for app.js

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -7,7 +7,8 @@ describe('<App />', () => {
   const mountWrapper = mount(<App/>);
   const newWrapper = new App();
   const wrapper = shallow(<App />);
-
+  const myArray = [0,2,5,9];
+  
   /* Component render */
   it('renders App without crashing', () => { 
     shallow(<App />);
@@ -32,23 +33,22 @@ describe('<App />', () => {
     expect(newWrapper.state.xIsNext).toBeTruthy();
   });
 
-  it('Expect history of squares to be null on init', () => {    
+  it('Expect history state to be 1 on init', () => {    
     expect(newWrapper.state.history.length).toEqual(1);
   });
 
   /* handleClick function */  
-  it('Expect on calling handleClick stepNumber will change', () => {
+  it('On calling handleClick stepNumber will change', () => {
     const wrapper = shallow(<App />);
     const realComponentInstance = wrapper.instance();
     expect(realComponentInstance.state.stepNumber).toEqual(0);
       
     realComponentInstance.handleClick(1);
     wrapper.update();
-
     expect(realComponentInstance.state.stepNumber).toEqual(1);
   });
 
-  it('Expect on calling handleClick xIsNext will change', () => {
+  it('On calling handleClick xIsNext will change', () => {
     const wrapper = shallow(<App />);    
     const realComponentInstance = wrapper.instance();
 
@@ -56,11 +56,10 @@ describe('<App />', () => {
       
     realComponentInstance.handleClick(1);
     wrapper.update();
-
     expect(realComponentInstance.state.xIsNext).toBeFalsy();
   });
 
-  it('Expect on calling handleClick xIsNext will change', () => {
+  it('On calling handleClick history will change', () => {
     const wrapper = shallow(<App />);    
     const realComponentInstance = wrapper.instance();
 
@@ -68,9 +67,124 @@ describe('<App />', () => {
       
     realComponentInstance.handleClick(1);
     wrapper.update();
-
     expect(realComponentInstance.state.history.length).toEqual(2);  
   });
 
   /* jumpTo function */
+
+  it('On calling jumpTo step one stepNumber will reset', () => {
+    const wrapper = shallow(<App />);    
+    const realComponentInstance = wrapper.instance();
+
+    realComponentInstance.handleClick(1);
+    wrapper.update();
+    expect(realComponentInstance.state.stepNumber).toEqual(1);
+
+    realComponentInstance.jumpTo(0);
+    wrapper.update();
+    expect(realComponentInstance.state.stepNumber).toEqual(0);  
+  });  
+
+  it('On calling jumpTo step one xIsNext will reset', () => {
+    const wrapper = shallow(<App />);    
+    const realComponentInstance = wrapper.instance();
+
+    realComponentInstance.handleClick(1);
+    wrapper.update();
+    expect(realComponentInstance.state.xIsNext).toBeFalsy();
+
+    realComponentInstance.jumpTo(0);
+    wrapper.update();
+    expect(realComponentInstance.state.xIsNext).toBeTruthy();  
+  });  
+  
+  it('On calling jumpTo step one the history will reset', () => {
+    const wrapper = shallow(<App />);    
+    const realComponentInstance = wrapper.instance();
+
+    realComponentInstance.handleClick(1);
+    wrapper.update();
+    expect(realComponentInstance.state.history.length).toEqual(2);
+
+    realComponentInstance.jumpTo(0);
+    wrapper.update();
+    expect(realComponentInstance.state.history.length).toEqual(1);  
+  });  
+
+  it('On calling jumpTo step three will change stepNumber', () => {
+    const wrapper = shallow(<App />);    
+    const realComponentInstance = wrapper.instance();
+
+    for (let value of myArray) {
+      realComponentInstance.handleClick(value);
+    }
+    wrapper.update();
+    expect(realComponentInstance.state.stepNumber).toEqual(4);
+
+    realComponentInstance.jumpTo(3);
+    wrapper.update();   
+    expect(realComponentInstance.state.stepNumber).toEqual(3);
+  });  
+
+  it('On calling jumpTo step three will change xIsNext', () => {
+    const wrapper = shallow(<App />);    
+    const realComponentInstance = wrapper.instance();
+
+    for (let value of myArray) {
+      realComponentInstance.handleClick(value);
+    }
+    wrapper.update();
+    expect(realComponentInstance.state.xIsNext).toBeTruthy();
+
+    realComponentInstance.jumpTo(3);
+    wrapper.update();
+    expect(realComponentInstance.state.xIsNext).toBeFalsy();  
+  });
+
+  /* Reset data function */
+
+  it('On calling jumpTo stepNumber will equal 1', () => {
+    const wrapper = shallow(<App />);    
+    const realComponentInstance = wrapper.instance();
+
+    for (let value of myArray) {
+      realComponentInstance.handleClick(value);
+    }
+    wrapper.update();
+    expect(realComponentInstance.state.stepNumber).toEqual(4);
+
+    realComponentInstance.resetData();
+    wrapper.update();   
+    expect(realComponentInstance.state.stepNumber).toEqual(0);
+  });  
+
+  it('On calling resetData xIsNext will be true', () => {
+    const wrapper = shallow(<App />);    
+    const realComponentInstance = wrapper.instance();
+
+    for (let value of myArray) {
+      realComponentInstance.handleClick(value);
+    }
+    wrapper.update();
+    expect(realComponentInstance.state.xIsNext).toBeTruthy();
+
+    realComponentInstance.resetData();
+    wrapper.update();
+    expect(realComponentInstance.state.xIsNext).toBeTruthy();  
+  });
+
+  it('On calling resetData history will equal one', () => {
+    const wrapper = shallow(<App />);    
+    const realComponentInstance = wrapper.instance();
+
+    for (let value of myArray) {
+      realComponentInstance.handleClick(value);
+    }
+    wrapper.update();
+    expect(realComponentInstance.state.history.length).toEqual(5);
+
+    realComponentInstance.resetData();
+    wrapper.update();
+    expect(realComponentInstance.state.history.length).toEqual(1);  
+  });  
 });
